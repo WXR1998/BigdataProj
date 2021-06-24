5e5eb2105fe45db1e4634b6e016001e38d46e48b
Xuanrun Wang <810158407@qq.com>
2021-06-24 19:40:32 +0800

    upd

--- src/kvstore.cpp
+++ src/kvstore.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #include <unistd.h>
 #include <string>
+#include <cstdlib>
+#include <cassert>
 
 #include "kvstore.hpp"
 #include "config.hpp"
@@ -11,24 +13,24 @@ KeyInfo::KeyInfo(){
     start_byte = -1;
     del_flag = KeyState::DELETE;
 }
-KeyInfo::KeyInfo(std::string key, int start_byte, KeyState del_flag):
-    key(key), start_byte(start_byte), del_flag(del_flag){
+KeyInfo::KeyInfo(std::string key, int start_byte, int row_num, KeyState del_flag):
+    key(key), start_byte(start_byte), row_num(row_num), del_flag(del_flag){
 }
 void KeyInfo::print(){
     printf("Key: %s, start byte: %d, del flag: %d\n", key.c_str(), start_byte, del_flag);
 }
 
-OperationLog::OperationLog(KeyState fr_del_flag, int fr_value, KeyState to_del_flag, int to_value):
-    fr_del_flag(fr_del_flag), fr_value(fr_value), to_del_flag(to_del_flag), to_value(to_value){
+OperationLog::OperationLog(std::string key, KeyState fr_del_flag, int fr_value, KeyState to_del_flag, int to_value):
+    key(key), fr_del_flag(fr_del_flag), fr_value(fr_value), to_del_flag(to_del_flag), to_value(to_value){
 }
 
 KV::KV(){
     key = "";
     value = 0;
-    lock = LockState::UNLOCK;
+    del_flag = KeyState::EXIST;
 }
-KV::KV(std::string key, int value): key(key), value(value){
-    lock = LockState::UNLOCK;
+KV::KV(std::string key, int value, KeyState del_flag): key(key), value(value), del_flag(del_flag){
+    del_flag = KeyState::EXIST;
 }
 
 KVStore::KVStore(){
@@ -36,6 +38,8 @@ KVStore::KVStore(){
     data_handle = NULL;
     key_count = 0;
     key_info.clear();
+    DaemonKeyMappingFileInit();
+    DaemonDataFileInit();
 }
 KVStore::~KVStore(){
     if (key_mapping_handle)
@@ -54,7 +58,7 @@ void KVStore::DaemonDataFileInit(){
             fseek(data_handle, start_byte, SEEK_SET);
             IntValueUnion vu;
             fread(vu.c, sizeof(IntValueUnion), 1, data_handle);
-            kvs[key] = KV(key, vu.value);
+            kvs[key] = KV(key, vu.value, kv.second.del_flag);
             printf("%s: %d\n", key.c_str(), vu.value);
         }
     }else{
@@ -73,7 +77,7 @@ void KVStore::DaemonKeyMappingFileInit(){
             int start_byte; 
             KeyState del_flag;
             fscanf(key_mapping_handle, "%s%d%d", s, &start_byte, &del_flag);
-            key_info[std::string(s)] = KeyInfo(std::string(s), start_byte, del_flag);
+            key_info[std::string(s)] = KeyInfo(std::string(s), start_byte, i+1, del_flag);
         }
         for (auto i: key_info)
             i.second.print();
@@ -89,4 +93,83 @@ void KVStore::DropDatabase(){
         fclose(data_handle);
     remove(key_mapping_path);
     remove(data_path);
+    kvs.clear();
+}
+void KVStore::ModifyKey(std::string key, KeyState del_flag){
+    if (key_info.find(key) != key_info.end()){
+        // TODO
+    }else{
+        // TODO 新增一行
+    }
+}
+
+void KVStore::Update(std::string key, int value, KeyState state, bool need_lock){
+    if (need_lock)
+        lock.lock();
+    std::pair<KeyState, int> current_value = Get(key, false);
+    kvs[key] = KV(key, value, state);
+    // TODO 落盘
+    if (need_lock)
+        lock.unlock();
+    else{   // 不需要锁，说明这个update操作是在一个Trans内部，需要记录日志以备还原
+        OperationLog l(key, current_value.first, current_value.second, state, value);
+        log.push(l);
+    }
+}
+bool KVStore::Add1(std::string key, bool need_lock){
+    if (need_lock)
+        lock.lock();
+    std::pair<KeyState, int> current_value = Get(key, false);
+    bool ret = true;
+    if (current_value.first == KeyState::EXIST)
+        Update(key, current_value.second + 1, KeyState::EXIST, false);
+    else
+        ret = false;
+    if (need_lock)
+        lock.unlock();
+    return ret;
+}
+bool KVStore::Delete(std::string key, bool need_lock){
+    if (need_lock)
+        lock.lock();
+    std::pair<KeyState, int> current_value = Get(key, false);
+    bool ret = true;
+    if (current_value.first == KeyState::EXIST)
+        Update(key, 0, KeyState::DELETE, false);
+    else
+        ret = false;
+    if (need_lock)
+        lock.unlock();
+    return ret;
+}
+std::pair<KeyState, int> KVStore::Get(std::string key, bool need_lock){
+    if (need_lock)
+        lock.lock();
+    std::pair<KeyState, int> ans;
+    if (kvs.find(key) != kvs.end())
+        ans = std::make_pair(kvs[key].del_flag, kvs[key].value);
+    else
+        ans = std::make_pair(KeyState::DELETE, 0);
+    if (need_lock)
+        lock.unlock();
+    return ans;
+}
+void KVStore::StartTrans(){
+    lock.lock();
+    while (log.size())
+        log.pop();
+}
+void KVStore::CommitTrans(){
+    lock.unlock();
+}
+void KVStore::AbortTrans(){
+    while (log.size()){
+        OperationLog l = log.top();
+        log.pop();
+        assert(kvs[l.key].del_flag == l.to_del_flag);
+        assert(kvs[l.key].value == l.to_value);
+        kvs[l.key] = KV(l.key, l.fr_value, l.fr_del_flag);
+        // TODO 落盘
+    }
+    lock.unlock();
 }
--- src/kvstore.hpp
+++ src/kvstore.hpp
@@ -2,12 +2,14 @@
 #include <map>
 #include <string>
 #include <stack>
+#include <mutex>
+#include <cstdlib>
 
 enum KeyState{
     EXIST = 0, DELETE = 1
 };
-enum LockState{
-    UNLOCK = 0, LOCK = 1
+enum TransState{
+    FREE = 0, RUNNING = 1
 };
 union IntValueUnion{
     int value;
@@ -17,9 +19,10 @@ union IntValueUnion{
 struct KeyInfo{
     std::string key;
     int start_byte;
+    int row_num;
     KeyState del_flag;
     KeyInfo();
-    KeyInfo(std::string key, int start_byte, KeyState del_flag);
+    KeyInfo(std::string key, int start_byte, int row_num, KeyState del_flag);
     void print();
 };
 
@@ -28,18 +31,19 @@ struct OperationLog{
         操作日志，记录每个操作操作前的值和操作后的值，以及删除标记
         插入一个kv时，del_flag是从1到0
     */
+    std::string key;
     int fr_value, to_value;
     KeyState fr_del_flag, to_del_flag;
-    OperationLog(KeyState fr_del_flag, int fr_value, KeyState to_del_flag, int to_value);
+    OperationLog(std::string key, KeyState fr_del_flag, int fr_value, KeyState to_del_flag, int to_value);
 };
 
 struct KV{
     std::string key;
     int value;
-    LockState lock;
+    KeyState del_flag;
 
     KV();
-    KV(std::string key, int value);
+    KV(std::string key, int value, KeyState del_flag);
 };
 
 class KVStore{
@@ -48,13 +52,28 @@ private:
     FILE *data_handle;
     std::map <std::string, KeyInfo> key_info;
     std::map <std::string, KV> kvs;
+    std::mutex lock;
+
     // 记录Trans中的操作
     std::stack <OperationLog> log;
+    bool need_lock;
+
     int key_count;
 public:
     KVStore();
     ~KVStore();
     void DaemonKeyMappingFileInit();
     void DaemonDataFileInit();
+    void ModifyKey(std::string key, KeyState del_flag);
+    void ModifyData(std::string key, int value);
     void DropDatabase();
+
+    // 以下操作方法都需要传入need_lock
+    void Update(std::string key, int value, KeyState state, bool need_lock);
+    bool Add1(std::string key, bool need_lock);
+    bool Delete(std::string key, bool need_lock);
+    std::pair<KeyState, int> Get(std::string key, bool need_lock);
+    void StartTrans();
+    void CommitTrans();
+    void AbortTrans();
 };
